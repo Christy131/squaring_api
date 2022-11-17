3732dd275d78f34c46f09ae68f2cf54c50fbc200
Christy Hayter <christyhayter@Christys-MBP.lan>
2022-11-16 21:32:24 -0500

    django-projects

---
+++ squaring_api/squaring/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+# Create your views here.
+
+
+class SquaringView(View):
+    def get(self, request, number):
+        return HttpResponse(number ** 2)
